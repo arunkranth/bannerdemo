236eb40e585fa162bd5c20b513b5e261b0efa0c1
Arun Kranth Rayudu <arunkranth@gmail.com>
2013-03-26 23:12:42 +0530

    banner demo files

---
+++ app/code/local/Arun/Bannerslider/Model/Mysql4/Bannerslider.php
@@ -0,0 +1,9 @@
+<?php
+
+class Arun_Bannerslider_Model_Mysql4_Bannerslider extends Mage_Core_Model_Mysql4_Abstract
+{
+    public function _construct()
+    {    
+        $this->_init('bannerslider/bannerslider', 'bannerslider_id');
+    }
+}
